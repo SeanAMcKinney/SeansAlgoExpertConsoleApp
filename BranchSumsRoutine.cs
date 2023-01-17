95ef34d410521b347b6833c3f37b4f35948957bf
Sean McKinney <seanamckinney8@gmail.com>
2023-01-17 12:50:30 -0600

    working on creating a linked list and adding test data to run BranchSumsAlgorithm

--- BranchSumsRoutine.cs
+++ BranchSumsRoutine.cs
@@ -1,4 +1,5 @@
 ﻿using AlgoExpertAlgorithmsLibrary;
+using System.Collections.Generic;
 
 namespace SeansAlgoExpertConsoleApp
 {
@@ -8,40 +9,44 @@ namespace SeansAlgoExpertConsoleApp
         {
             Messages.BranchSumsInfo();
             Messages.HowManyNodesInYourBinaryTree();
-            int numberOfNodes = Convert.ToInt32(Console.ReadLine());
-            BranchSumsAlgorithm.BinaryTree[] nodes = new BranchSumsAlgorithm.BinaryTree[numberOfNodes].ToArray();
-
-            for (int i = 0; i < numberOfNodes; i++)
-            {
-                //Messages.EnterNodeId();
-                //string id = i.ToString();
-                
-                Messages.BinaryTreeLeftValue();
-                string? left = Console.ReadLine();
-                Messages.BinaryTreeRightValue();
-                string? right = Console.ReadLine();
-                Messages.EnterNodeValue();
-                int value = Convert.ToInt32(Console.ReadLine());
-
-                object node = new {
-                    Left = left,
-                    Right = right,
-                    Value = value,
-                };
-
-                nodes[i] = node.ToString();
-
-                i++;
-            }
-
-            Messages.EnterYourRoot();
-            var root = Console.ReadLine();
-
-
-
+            SingleLinkedListCreator.SingleLinkedList linkedList = new SingleLinkedListCreator.SingleLinkedList();
+            linkedList.
         }
     }
 }
 
+
+// ***** First attempt at creating a routine to take in data and us algorithm *****
+
+
+//int numberOfNodes = Convert.ToInt32(Console.ReadLine());
+//BranchSumsAlgorithm.BinaryTree[] nodes = new BranchSumsAlgorithm.BinaryTree[numberOfNodes].ToArray();
+
+//for (int i = 0; i < numberOfNodes; i++)
+//{
+//    //Messages.EnterNodeId();
+//    //string id = i.ToString();
+
+//    Messages.BinaryTreeLeftValue();
+//    string? left = Console.ReadLine();
+//    Messages.BinaryTreeRightValue();
+//    string? right = Console.ReadLine();
+//    Messages.EnterNodeValue();
+//    int value = Convert.ToInt32(Console.ReadLine());
+
+//    object node = new
+//    {
+//        Left = left,
+//        Right = right,
+//        Value = value,
+//    };
+
+//    //nodes[i] = node.ToString();
+
+//    i++;
+//}
+
+//Messages.EnterYourRoot();
+//var root = Console.ReadLine();
 
 
---
+++ SingleLinkedListCreator.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeansAlgoExpertConsoleApp
+{
+    public class SingleLinkedListCreator
+    {
+        internal class Node
+        {
+            internal int data;
+            internal Node? next;
+            public Node(int d)
+            {
+                data = d;
+                next = null;
+            }
+        }
+
+        internal class SingleLinkedList
+        {
+            internal Node head;
+
+
+            internal void InsertFront(SingleLinkedList singlyList, int new_data)
+            {
+                Node new_node = new Node(new_data);
+                new_node.next = singlyList.head;
+                singlyList.head = new_node;
+            }
+
+            internal void InsertLast(SingleLinkedList singlyList, int new_data)
+            {
+                Node new_node = new Node(new_data);
+                if (singlyList.head == null)
+                {
+                    singlyList.head = new_node;
+                    return;
+                }
+                Node lastNode = GetLastNode(singlyList);
+                lastNode.next = new_node;
+            }
+
+            internal Node GetLastNode(SingleLinkedList singlyList)
+            {
+                Node temp = singlyList.head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                return temp;
+            }
+
+            internal void InsertAfter(Node prev_node, int new_data)
+            {
+                if (prev_node == null)
+                {
+                    Console.WriteLine("The given previous node Cannot be null");
+                    return;
+                }
+                Node new_node = new Node(new_data);
+                new_node.next = prev_node.next;
+                prev_node.next = new_node;
+            }
+        }
+    }
+}
